1ac133baabbb01ec8808bb10d4362fd8680ec4c9
ParasThakur199 <parasjamwal19@gmail.com>
2023-11-15 18:08:14 +0530

    add getAllComments

--- CrmApplication/src/main/java/com/thoughtpearls/controller/CommentsController.java
+++ CrmApplication/src/main/java/com/thoughtpearls/controller/CommentsController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class CommentsController {
 
@@ -32,5 +34,10 @@ public class CommentsController {
         return new ResponseEntity<>("Comment deleted successfully", HttpStatus.OK);
     }
 
+    @GetMapping("/getAll")
+    public ResponseEntity<List<CommentsResponseDto>> getAllComments() {
+        List<CommentsResponseDto> comments = commentsService.getAllComments();
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
 
 }
--- CrmApplication/src/main/java/com/thoughtpearls/mapper/CommentsMapper.java
+++ CrmApplication/src/main/java/com/thoughtpearls/mapper/CommentsMapper.java
@@ -2,9 +2,13 @@ package com.thoughtpearls.mapper;
 
 import com.thoughtpearls.dto.CommentsRequestDto;
 import com.thoughtpearls.dto.CommentsResponseDto;
+import com.thoughtpearls.dto.LeadResponseDto;
 import com.thoughtpearls.model.Comments;
+import com.thoughtpearls.model.Lead;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CommentsMapper {
@@ -14,4 +18,8 @@ public interface CommentsMapper {
     CommentsResponseDto entityToDto(Comments comments);
 
     void updateEntityFromDto(CommentsRequestDto commentsRequestDto, @MappingTarget Comments comments);
+
+    List<CommentsResponseDto> listOfEntitiesToListOfDto(List<Comments> commentsList);
+
+    List<Comments> dtoToEntity(List<CommentsResponseDto> commentsResponseDtos);
 }
