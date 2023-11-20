aa3f3762a5cb53434f07aaafa97d6ee6556a60c1
deepakagarwal01 <deepakagarwal014@gmail.com>
2023-11-20 16:07:38 +0530

    initial

--- CrmApplication/src/main/java/com/thoughtpearls/repository/LeadRepository.java
+++ CrmApplication/src/main/java/com/thoughtpearls/repository/LeadRepository.java
@@ -1,9 +1,6 @@
 package com.thoughtpearls.repository;
 
 import com.thoughtpearls.model.Lead;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
